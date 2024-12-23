4dbb90323b5976f898a2aefccc52c3d0a2de9bab
zhouhuajian <zhouhuajian@company.com>
2024-12-23 17:33:04 +0800

    04 创建住房位置组件

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styles: ``
+})
+export class HousingLocationComponent {
+
+}
