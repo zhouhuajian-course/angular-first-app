a15bc66478fd95c019964e1b03a1315885b447b1
zhouhuajian <zhouhuajian@company.com>
2024-12-23 16:35:57 +0800

    03 创建首页组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import {Component} from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
+// 根组件 -> Home
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomeComponent],
   template: `
-    <h1>Hello World!!!</h1>
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+
+}
