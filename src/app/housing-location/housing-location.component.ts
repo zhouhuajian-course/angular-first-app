515d92e0fc677b88d1b84ca930722dd1e6afa8c4
zhouhuajian <zhouhuajian@company.com>
2024-12-27 18:10:06 +0800

    08 给组件模板添加文本插值

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -5,11 +5,20 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   imports: [],
   template: `
-    <p>
-      housing-location works! {{housingLocation.name}} {{housingLocation.city}}
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
-  styles: ``
+  // styles: ``
+  styleUrls: ['./housing-location.component.css'],
+
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
