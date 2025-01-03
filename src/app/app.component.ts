80ecc648231ef7b4f5cc707a75741f888f60ef6d
zhouhuajian <zhouhuajian@company.com>
2025-01-03 14:48:42 +0800

    10 添加路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from "./details/details.component";
+import { RouterModule } from '@angular/router';
 // 根组件 -> Home
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [/*HomeComponent, DetailsComponent,*/ RouterModule],
   template: `
     <main>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
+        <!-- 显示这个标签的下面 http://localhost:4200/ -->
+        <!-- URL / app-home /details/1 app-details -->
+        <!-- <app-home></app-home>
+        <app-details></app-details> -->
       </section>
     </main>
   `,
---
+++ src/app/details/details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  template: `
+    <p>
+      details works! 详情页
+    </p>
+  `,
+  styles: ``
+})
+export class DetailsComponent {
+
+}
