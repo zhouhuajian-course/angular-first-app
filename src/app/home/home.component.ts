4f54ee41bddc8f7a057633906c42fb87bd71ac51
zhouhuajian <zhouhuajian@company.com>
2025-01-08 09:31:04 +0800

    15 使用 HTTP 接口获取住房数据

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,8 +29,12 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations()
-    this.filteredLocationList = this.housingLocationList
+  //   this.housingLocationList = this.housingService.getAllHousingLocations()
+  //   this.filteredLocationList = this.housingLocationList
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
   filterResults(city: string) {
     if (!city) {
